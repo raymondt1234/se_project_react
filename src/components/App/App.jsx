f0b97add8b7350db3b3f52e5d226117f0e26b91b
Raymond Terry <raymondt1234@gmail.com>
2025-03-09 10:10:27 -0700

    add correct font to the Header

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,10 @@
-import "./App.css";
-import Header from "../Header/Header";
+import './App.css';
+import Header from '../Header/Header';
 
 function App() {
   return (
-    <div className="page">
-      <div className="page__content">
+    <div className='page'>
+      <div className='page__content'>
         <Header />
       </div>
     </div>
